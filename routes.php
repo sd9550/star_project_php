911eaf4ccc6a7dc50dce5babcdde4491f323791f
Stefan <stefan.drozdek@gmail.com>
2024-09-27 01:42:44 -0500

    Delete routes.php

--- routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-$router->get('/', 'HomeController@index');
-$router->get('/stars', 'StarController@index');
-$router->get('/stars/create', 'StarController@create');
-$router->get('/stars/{id}', 'StarController@show');
-$router->post('/stars', 'StarController@store');
